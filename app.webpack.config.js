a1cedbb72180fdb67bf2e399be1d6ddb795aa504
Dimitri <couwenberg@gmail.com>
2018-02-15 17:16:51 +1100

    updated npm run dev

--- app.webpack.config.js
+++ app.webpack.config.js
@@ -1,14 +1,20 @@
 var webpack = require('webpack');
+var path = require('path');
 module.exports = {
-  entry: {
-    app: ['./app'],
-  },
-  output: {
-    filename: 'app.bundle.js',
-    path: '/Users/dimitri/Downloads/wpdll/build/',
-  },
-  plugins: [new webpack.DllReferencePlugin({
-    context: '.',
-    manifest: require('./build/vendor-manifest.json'),
-  })]
+    entry: {
+        app: ['./app'],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "build"),
+        compress: true,
+        port: 9000
+    },
+    output: {
+        filename: 'app.bundle.js',
+        path: '/Users/dimitri/Downloads/wpdll/build/',
+    },
+    plugins: [new webpack.DllReferencePlugin({
+        context: '.',
+        manifest: require('./build/vendor-manifest.json'),
+    })]
 };
